1a4ee31a55941f6c3bca74a2f4c0976f1a71d599
ayaAhmedEsmail <76392909+ayaAhmedEsmail@users.noreply.github.com>
2021-02-20 19:57:48 +0200

    Creat Trip XML

---
+++ app/src/main/java/iti/intake41/myapplication/createtrip/view/CreateTrip.java
@@ -0,0 +1,26 @@
+package iti.intake41.myapplication.createtrip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import iti.intake41.myapplication.R;
+
+public class CreateTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_trip);
+    }
+
+    public void timePicker(View view) {
+    }
+
+    public void datePicker(View view) {
+    }
+
+    public void onCheckboxClicked(View view) {
+    }
+}
